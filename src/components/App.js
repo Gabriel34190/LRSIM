9e33c1431c1d71ee39c5c61b8ee2ed31a217f91a
Gabriel34190 <gabriel.rouchon@epitech.eu>
2024-10-21 10:12:16 +0200

    Début configuration FireBase

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import Appartements from './Appartements';
 import DetailsAppartement from './DetailsAppartement';
 import Owner from './Owner';  // Ajout de l'importation pour Owner
+import Connexion from './Connexion';
 import '../css/App.css';  // Mise à jour du chemin vers le CSS
 
 const App = () => {
@@ -13,7 +14,8 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/appartements" element={<Appartements />} />
                 <Route path="/details-appartement" element={<DetailsAppartement />} />
-                <Route path="/proprietaires" element={<Owner />} /> {/* Ajout de la route pour le composant Owner */}
+                <Route path="/proprietaires" element={<Owner />} />
+                <Route path="/connexion" element={<Connexion />} />
             </Routes>
         </Router>
     );
