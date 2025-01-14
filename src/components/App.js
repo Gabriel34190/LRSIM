f8b8fb37e3c9bbc373008c5c4b6a71026341e898
Gabriel34190 <gabriel.rouchon@epitech.eu>
2025-01-14 09:25:58 +0100

    attribution d'une page dynamique pr les apparts

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import LocationPage from './LocationPage';
 import AppartementDetailsPage from './AppartementDetailsPage';
-import Proprietaires from './Proprietaires';  // Assurez-vous que le nom est correct
+import Proprietaires from './Proprietaires';
 import Connexion from './Connexion';
-import '../css/App.css';  // Mise à jour du chemin vers le CSS
+import '../css/App.css';
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/details-appartement:appartementId" element={<AppartementDetailsPage />} />
-                <Route path="/proprietaires" element={<Proprietaires />} /> {/* Utilise Proprietaires ici */}
+                <Route path="/details-appartement/:appartementId" element={<AppartementDetailsPage />} />
+                <Route path="/proprietaires" element={<Proprietaires />} />
                 <Route path="/connexion" element={<Connexion />} />
                 <Route path="/locations/:id" element={<LocationPage />} /> {/* Route dynamique */}
-
             </Routes>
         </Router>
     );
